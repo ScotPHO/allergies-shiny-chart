805b8ca7010ce2e255c322618436cb423f3c530e
Jaime Villacampa <jaime.villacampa@phs.scot>
2021-08-10 11:36:09 +0100

    Moving data prep to its own script

---
+++ data_preparation.R
@@ -0,0 +1,14 @@
+# Preparing data - not needed unless new data coming through
+library(tidyr)
+library(dplyr)
+
+data_allergy <- readRDS("/PHI_conf/ScotPHO/Website/Topics/Allergy/december2020_update/allergy_scotland_chart.rds") %>%
+  mutate(rate = round(rate, 1), # round numbers more (one decimal place)
+         numerator = case_when(numerator < 5 ~ NA_real_,
+                               TRUE ~ numerator)) %>%  # supression of under 5
+  gather(measure, value, -c(type, year)) %>%
+  mutate(measure = recode(measure, "numerator" = "Number", "rate" = "Rate"))
+
+saveRDS(data_allergy, "shiny_app/allergy_scotland_chart.rds")
+
+## END
--- shiny_app/global.R
+++ shiny_app/global.R
@@ -9,23 +9,7 @@ library(plotly) #charts
 library(shiny)
 library(shinymanager)
 
-
-# Preparing data - not needed unless new data coming through
-library(tidyr)
-# 
-# data_allergy <- readRDS("/PHI_conf/ScotPHO/Website/Topics/Allergy/december2020_update/allergy_scotland_chart.rds") %>%
-#   mutate(rate = round(rate, 1), # round numbers more (one decimal place)
-#          numerator = case_when(numerator < 5 ~ NA_real_,
-#                                TRUE ~ numerator)) %>%  # supression of under 5
-#   gather(measure, value, -c(type, year)) %>%
-#   mutate(measure = recode(measure, "numerator" = "Number", "rate" = "Rate"))
-
-#saveRDS(data_allergy, "data/allergy_scotland_chart.rds")
-#PRA Data - not to be published
-#saveRDS(data_allergy, "allergy_scotland_chart_PRA.rds")
-
-#data_allergy <- readRDS("allergy_scotland_chart.rds") 
-data_allergy <- readRDS("allergy_scotland_chart_PRA.rds") 
+data_allergy <- readRDS("allergy_scotland_chart.rds")
 
 #Use for selection of conditions
 condition_list <- sort(unique(data_allergy$type))
@@ -36,3 +20,4 @@ scotpho_logo <-  list(source ="https://raw.githubusercontent.com/ScotPHO/plotly-
                       xref = "paper", yref = "paper",
                       x= -0.09, y= 1.2, sizex = 0.22, sizey = 0.18, opacity = 1)
 
+## END
